8303fdb8c502af876d91e99a60ee1c095ee36fe2
Cory Parrish <coryparrish@Corys-MacBook-Pro.local>
2022-04-10 16:14:57 -0400

    Adds proposal link to main nav.

--- modules/rsvp/server/index.js
+++ modules/rsvp/server/index.js
@@ -2,47 +2,41 @@
 
 var api = require("../../main/server/api");
 var rsvpService = require("./services/rsvpService");
-var database = require("../../database");
 
 module.exports = {
   addApiRoutes : function (app) {
+    console.log("Adding 'rsvp' API routes.");
 
-    database.getConnection().then(function (db) {
-      console.log("Adding 'rsvp' API routes.");
+    app.get(api.getApiRoute("rsvp"), function (req, res) {
+      rsvpService.getAllNames().then(function (result) {
+        api.sendSuccess(res, result);
+      }).fail(function (err) {
+        api.sendError(res, err);
+      }).done();
+    });
 
-      app.get(api.getApiRoute("rsvp"), function (req, res) {
-        rsvpService.getAllNames(db).then(function (result) {
-          api.sendSuccess(res, result);
-        }).fail(function (err) {
-          api.sendError(res, err);
-        }).done();
-      });
+    app.get(api.getApiRoute("rsvp/responded"), function (req, res) {
+      rsvpService.getResponded().then(function () {
+        api.sendSuccess(res);
+      }).fail(function (err) {
+        api.sendError(res, err);
+      }).done();
+    });
 
-      app.get(api.getApiRoute("rsvp/responded"), function (req, res) {
-        rsvpService.getResponded(db).then(function () {
-          api.sendSuccess(res);
-        }).fail(function (err) {
-          api.sendError(res, err);
-        }).done();
-      });
+    app.post(api.getApiRoute("rsvp"), function (req, res) {
+      rsvpService.save(req.body.guest).then(function (result) {
+        api.sendSuccess(res, result);
+      }).fail(function (err) {
+        api.sendError(res, err);
+      }).done();
+    });
 
-      app.post(api.getApiRoute("rsvp"), function (req, res) {
-        rsvpService.save(req.body.guest, db).then(function (result) {
-          api.sendSuccess(res, result);
-        }).fail(function (err) {
-          api.sendError(res, err);
-        }).done();
-      });
-
-      app.post(api.getApiRoute("rsvp/passcode"), function (req, res) {
-        rsvpService.verifyPasscode(req.body.passcode).then(function (result) {
-          api.sendSuccess(res, result);
-        }).fail(function (err) {
-            api.sendError(res, err);
-          }).done();
-      });
-    }).fail(function (err) {
-      console.log(err);
+    app.post(api.getApiRoute("rsvp/passcode"), function (req, res) {
+      rsvpService.verifyPasscode(req.body.passcode).then(function (result) {
+        api.sendSuccess(res, result);
+      }).fail(function (err) {
+        api.sendError(res, err);
+      }).done();
     });
   }
 };
--- modules/rsvp/server/services/rsvpService.js
+++ modules/rsvp/server/services/rsvpService.js
@@ -1,93 +1,25 @@
 "use strict";
 
 var Q = require("q");
-var ObjectId = require("mongodb").ObjectID;
-var fs = require("fs");
 
 module.exports = {
-  getAllNames : function (db) {
+  getAllNames : function () {
     var deferred = Q.defer();
-    var collection = db.collection('guests');
-
-    collection.find().toArray(function(err, docs) {
-      if (err) {
-        deferred.reject(err);
-      }
-
-      deferred.resolve(docs);
-    });
-
+    deferred.resolve();
     return deferred.promise;
   },
-  getResponded : function (db) {
+  getResponded : function () {
     var deferred = Q.defer();
-    var collection = db.collection('guests');
-
-    var query = {
-      numGuests : {$exists : true}
-    };
-
-    collection.find(query).sort({timestamp: 1}).toArray(function(err, docs) {
-      if (err) {
-        deferred.reject(err);
-      }
-
-      helper.generateCsvSync(docs).then(function () {
-        deferred.resolve();
-      });
-    });
-
+    deferred.resolve();
     return deferred.promise;
   },
-  save : function (guests, db) {
+  save : function () {
     var deferred = Q.defer();
-
-    var collection = db.collection('guests');
-    guests._id = new ObjectId(guests._id);
-    collection.update( {_id : guests._id}, guests, function(err, docs) {
-      if (err) {
-        deferred.reject(err);
-      }
-      deferred.resolve();
-    });
-
+    deferred.resolve();
     return deferred.promise;
   },
   verifyPasscode : function (passcode) {
     var result = (passcode.toLowerCase() === "bigfluffydog" || passcode.toLowerCase() === "boobies") ? "VALID" : "INVALID";
     return new Q(result);
-  }
-};
-
-var helper = {
-  generateCsvSync: function (data) {
-    var deferred = Q.defer();
-
-    var fileName = "./public/csv/attendance.csv";
-
-    try {
-      if (!fs.existsSync("./public/csv")) {
-        fs.mkdirSync("./public/csv");
-      }
-      if (fs.existsSync(fileName)) {
-        fs.unlinkSync(fileName);
-      }
-
-      var csv = "NAME, # INVITED, # COMING, NOTES, ATTENDING, DATE RESPONDED\n";
-
-      data.forEach(function (doc) {
-        doc.notes = (doc.notes) ? doc.notes.replace(",", "") : "";
-        var timestamp = new Date(doc.timestamp || "");
-        var value = doc.name + "," + doc.numInvited + "," + doc.numGuests + "," + doc.notes + "," + doc.attending + "," + timestamp + "\n";
-        csv += value;
-      });
-      fs.appendFileSync(fileName, csv);
-      deferred.resolve();
-    } catch (err) {
-      console.log(err);
-      deferred.reject(err);
-    }
-
-    return deferred.promise;
   }
 };
